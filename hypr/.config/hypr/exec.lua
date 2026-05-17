2167e4f0307e95268c2b5337d60447223334ec83
kouchpouch <nakouchdoge@gmail.com>
2026-05-17 08:45:38 -0500

    Renamed hyprexec.lua to exec.lua for easier tab completion. Annotated and removed GTK3 theme settings

---
+++ hypr/.config/hypr/exec.lua
@@ -0,0 +1,20 @@
+hl.on("hyprland.start", function ()
+	hl.exec_cmd("systemctl --user start hyprpolkitagent")
+	hl.exec_cmd("systemctl --user start swaync")
+	hl.exec_cmd("/usr/lib/xdg-desktop-portal-hyprland")
+	hl.exec_cmd("/usr/lib/xdg-desktop-portal-gtk")
+	hl.exec_cmd("hyprctl dispatch workspace 1")
+
+	-- For gtk3 apps you need to install adw-gtk3 theme (sudo pacman -S adw-gtk-theme)
+	-- These 2 commands are not required if you use nwg-look to set your theme.
+	--[[
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
+		]]
+
+	hl.exec_cmd("sleep 1")   -- If this is not here, waybar can fail in weird ways.
+	hl.exec_cmd("waybar") 	 -- Status bar
+	hl.exec_cmd("hyprpaper") -- Wallpapers
+	hl.exec_cmd("sunsetr")   -- Color temperature changer in the AUR
+	hl.exec_cmd("sunsetr set static_temp=6500") -- Resets color on start
+end)
