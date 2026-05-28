6696f11295bd542bbd2afcb8b81f6a8207025b02
kouchpouch <nakouchdoge@gmail.com>
2026-05-28 09:29:07 -0500

    Override the default 'hexagon style' c logo, which is not the C logo, with the letter C and recolored to match the K&R book cover color

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,18 @@
 return {
 	{
+	'nvim-tree/nvim-web-devicons',
+		opts = {
+			override = {
+				c = {
+					icon = "",
+					color = "#A8B9CC",
+					name = "DevIconC"
+				}
+			}
+		}
+	},
+
+	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = {
 			'nvim-tree/nvim-web-devicons'
