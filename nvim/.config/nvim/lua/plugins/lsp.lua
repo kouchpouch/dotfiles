8d358ceede39db51f4ff178caa40003298265708
kouchpouch <nakouchdoge@gmail.com>
2026-06-14 09:27:38 -0500

    Enter key will no longer auto complete the LSP suggestion, use y

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -62,7 +62,7 @@ return {
 
 	-- Accept currently selected item. Set `select` to `false` to only confirm 
 	-- explicitly selected items.
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+--		['<CR>'] = cmp.mapping.confirm({ select = true }),
 	}),
 	sources = cmp.config.sources({
 	  { name = 'nvim_lsp' },
