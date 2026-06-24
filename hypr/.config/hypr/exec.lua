c6de84fbb037f868138e6316021fbf7973dad267
kouchpouch <nakouchdoge@gmail.com>
2026-06-24 07:18:06 -0500

    Fixed waybar weirdness with a delay

--- hypr/.config/hypr/exec.lua
+++ hypr/.config/hypr/exec.lua
@@ -14,10 +14,8 @@ hl.on("hyprland.start", function ()
 	hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
 		]]
 
-	hl.exec_cmd("sleep 1")   -- If this is not here, waybar can fail in weird ways.
 	hl.exec_cmd("hyprpaper") -- Wallpapers
 	hl.exec_cmd("sunsetr")   -- Color temperature changer in the AUR
 	hl.exec_cmd("sunsetr set static_temp=6500") -- Resets color on start
-	hl.exec_cmd("sleep 1")   -- If this is not here, waybar can fail in weird ways.
-	hl.exec_cmd("waybar") 	 -- Status bar
+	hl.exec_cmd("sleep 5 && waybar") 	 -- Status bar
 end)
